c9677bfed2697fc16564be36050805dd723b2624
fearthepink <stoffer.dylan@proton.me>
2025-11-16 21:24:31 -0600

    I: we blasting?

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+from player import Player
+
+
+
+
+def main():
+    print(f"Starting Asteroids with Pygame version {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+
+    ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    clock = pygame.time.Clock(); dt = 0 #creates a clock class item with dt(delta-time) counter
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        ship.draw(screen)
+        ship.update(dt)
+        pygame.display.flip()   # this gives us a refresh on the screen
+        dt = clock.tick(60) / 1000 # this logs the time between screen refreshes
+        print(dt)
+    return
+
+if __name__ == "__main__":
+    main()
